4a5d302576c9bd502e4f66a37333c12098821bca
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-11-10 17:55:50 +0100

    create titolo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <titolo msg="Ciao.... finalmente..." />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import titolo from "./components/titolo.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    titolo,
   },
 };
 </script>
